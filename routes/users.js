775673f8ce6a88a313c34e46d348c9088caf6805
Neil Chauhan <neilchauhan2@gmail.com>
2019-07-09 21:31:59 +0530

    authentication done for guests

---
+++ routes/users.js
@@ -0,0 +1,98 @@
+const route = require("express").Router();
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const config = require("config");
+const auth = require("../middleware/auth");
+
+// create new user
+route.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // validation
+    if (!name || !email || !password) {
+      return res.status(400).send("Please enter all credentials!");
+    }
+    // check for existing user
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).send("User already exists!");
+    }
+
+    // hashing the password
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password, salt);
+
+    const createdUser = await new User({
+      name,
+      email,
+      password: hash
+    });
+
+    const newUser = await createdUser.save();
+    jwt.sign({ id: newUser.id }, config.get("jwtSecret"), (err, token) => {
+      if (err) throw err;
+      res.send({
+        token,
+        user: {
+          id: newUser.id,
+          name: newUser.name,
+          email: newUser.email
+        }
+      });
+    });
+  } catch (error) {
+    throw error;
+  }
+});
+
+// Login
+route.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Simple validation
+    if (!email || !password) {
+      return res.status(400).send("Please enter all credentials!");
+    }
+
+    // check for existing user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send("User does not exist!");
+    }
+
+    // compare password
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      jwt.sign({ id: user.id }, config.get("jwtSecret"), (err, token) => {
+        if (err) throw err;
+        res.send({
+          token,
+          user: {
+            id: user.id,
+            name: user.name,
+            email: user.email
+          }
+        });
+      });
+    } else {
+      return res.status(400).send("Password incorrect!");
+    }
+  } catch (error) {
+    throw error;
+  }
+});
+
+// Get User
+route.get("/user", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.send(user);
+  } catch (error) {
+    throw error;
+  }
+});
+
+module.exports = route;
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/homes", require("./routes/homes"));
+app.use("/guests", require("./routes/users"));
 
 app.get("/", (req, res) => {
   res.send(`
