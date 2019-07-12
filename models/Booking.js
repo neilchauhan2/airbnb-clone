a974ddda75c1f77f90d77402da3caf5c0f1d1ff9
Neil Chauhan <neilchauhan2@gmail.com>
2019-07-12 14:21:07 +0530

    Bookings schema updated

--- models/Booking.js
+++ models/Booking.js
@@ -1,19 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
 
 const BookingSchema = new Schema({
   customerName: {
     type: String,
     required: true
   },
+  customerId: ObjectId,
+  hostId: ObjectId,
   home: {
     location: String,
     name: String
   },
   host: {
     hostname: String,
-    location: String,
-    description: String
+    location: String
   },
   checkinDate: {
     type: Date
